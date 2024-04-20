9592c4b6f204d33039215785ccf0404a88666b8d
leocechellav@gmail.com <Leonardo>
2024-04-20 12:31:45 -0300

    Keycloak configuration added

--- src/main/java/org/ls/tweetpoints/GreetingResource.java
+++ src/main/java/org/ls/tweetpoints/GreetingResource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
@@ -12,5 +13,11 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/secure")
+    public Response getSecureData() {
+        return Response.ok("Este é um endpoint seguro!").build();
     }
 }
